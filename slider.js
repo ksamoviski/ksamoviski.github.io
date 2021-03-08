61bfc35493da63bf0b8934346e825b6d3a7ef9fb
kfiddle <kenjfiddle@gmail.com>
2021-03-08 16:29:38 -0500

    put all projects in js now, and adjusted slider

--- slider.js
+++ slider.js
@@ -3,30 +3,41 @@ const arrowRight = document.getElementById('arrow-right');
 const arrowLeft = document.getElementById('arrow-left');
 
 
-
-const slides = Array.from(document.querySelectorAll('.project__image'));
-
-
-
 let imagesSources = ["assets/bab.jpg", "assets/skypond.jpg", "assets/naturejournal.jpg",
-    "assets/berlin2019smaller.JPG", "assets/bryce2018smaller.JPG", "assets/grace2019smaller.JPG"];
+"assets/berlin2019smaller.JPG", "assets/bryce2018smaller.JPG", "assets/grace2019smaller.JPG"];
 
 
-for (let i = 0; i < slides.length; i++) {
-    slides[i].src = imagesSources[i];
+
+for (let i = 0; i < imagesSources.length; i++) {
+    let outerProjectDiv = document.createElement('div');
+    let innerProjectDiv = document.createElement('div');
+    let image = document.createElement('img');
+
+    outerProjectDiv.class = "single__project";
+    outerProjectDiv.style.margin = "1vw";
+
+    innerProjectDiv.class = "project__container";
+
+    image.class = "project__image";
+    image.src = imagesSources[i];
+    image.style.width = "25vw";
+
+    innerProjectDiv.appendChild(image);
+    outerProjectDiv.appendChild(innerProjectDiv);
+    carousel.appendChild(outerProjectDiv);
 }
-
 
 
 const forward = () => {
-    carousel.style.left = "-105%";
+    carousel.style.left = "-102%";
 }
 
 const backward = () => {
-    carousel.style.left = "0%";
+    carousel.style.left = "-2%";
 }
 
 
+backward();
 arrowRight.addEventListener('click', forward);
 arrowLeft.addEventListener('click', backward);
 
